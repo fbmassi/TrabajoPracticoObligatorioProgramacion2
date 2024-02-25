13531f860f300464639f0972e15196c5e16bb42f
Franco <francomassi1@gmail.com>
2024-02-25 17:35:20 -0300

    .

--- src/implementacionesEjercicios/Ejercicio3_MultiPila.java
+++ src/implementacionesEjercicios/Ejercicio3_MultiPila.java
@@ -1,5 +1,38 @@
 package implementacionesEjercicios;
 
-public class Ejercicio3_MultiPila {
+import interfaces.MultiPilaTDA;
+import interfaces.PilaTDA;
+
+public class Ejercicio3_MultiPila implements MultiPilaTDA{
+
+	@Override
+	public void apilar(PilaTDA valores) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void desapilar(PilaTDA valores) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PilaTDA tope(int cantidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void inicializarPila() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean pilaVacia() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
